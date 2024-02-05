85e14c7c4748e05c8bc95e50adb7820d4adadb40
João Pinto <pinto@fatska.xyz>
2024-02-05 18:34:32 +0000

    nvim: add some bindings for telescope, trouble and nvimtree

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -108,4 +108,23 @@ cmp.setup({
   }
 })
 
+-- telescope bindings
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- trouble bindings
+vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
+vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
+vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
+vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
+vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
+vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
+
+-- nvimtree bindings
+vim.keymap.set('n', "<leader>e", function() require("nvim-tree.api").tree.toggle() end)
+
+-- set theme
 vim.cmd.colorscheme "catppuccin-mocha"
