c9c5a773ef834c8fba740bfc33de4d34156814a2
jpinto <pinto@fatska.xyz>
2024-08-09 14:45:54 +0100

    yadm: add flatpak package install routine to 55-flatpak

--- .config/yadm/bootstrap.d/55-flatpak.sh
+++ .config/yadm/bootstrap.d/55-flatpak.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+printf "Starting Flatpak setup\n"
+
 FLATHUB_REPO="https://dl.flathub.org/repo/flathub.flatpakrepo"
 
 # Ensure Flatpak is installed
@@ -18,4 +20,24 @@ fi
 printf "Setting Flathub repo\n"
 flatpak remote-add --if-not-exists flathub ${FLATHUB_REPO}
 
+# Check if flatpak packages are installed
+printf "Checking %s/flatpak.txt for packages to install...\n" "${PACKAGE_DIR}"
+packages_to_install=()
+mapfile -t package_list < <( grep -vE '^$|#' "${PACKAGE_DIR}/flatpak.txt" )
+for package in "${package_list[@]}"; do
+	if ! flatpack list | grep -q "$package"; then
+		packages_to_install+=("${package}")
+	fi
+done
 
+# install the missing packages
+if [[ "${#packages_to_install[@]}" -gt 0 ]]; then
+	printf "Installing packages: %s\n" "${packages_to_install[*]}"
+	if [ $EUID != 0 ]; then
+		printf "%s\n" "${packages_to_install[@]}" | xargs sudo flatpak install --noninteractive
+	else
+		prints "%s\n" "${packages_to_install[@]}" | xargs flatpak install --noninteractive
+	fi
+fi
+
+printf "End Flatpak Setup\n"
