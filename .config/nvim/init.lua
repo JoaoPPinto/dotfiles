aac8877615b8052315fdeaa3122971d853909d6a
jpinto <pinto@fatska.xyz>
2024-08-20 11:15:02 +0100

    nvim: remove custom config, change to kickstart nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,133 +1,23 @@
--- vim.g.mapleader = '\'
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
-vim.g.loaded_netwr = 1
-vim.g.loaded_netrwPlugin = 1
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
 
--- Bootstrap Plugin Manager (lazy.nvim)
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
+-- [[ Setting options ]]
+require 'options'
 
--- Plugins
-require("lazy").setup('plugins')
+-- [[ Basic Keymaps ]]
+require 'keymaps'
 
-local on_attach = function(ev)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+-- [[ Install `lazy.nvim` plugin manager ]]
+require 'lazy-bootstrap'
 
-  -- Buffer local mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local opts = { buffer = ev.buf }
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-  vim.keymap.set('n', '<space>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, opts)
-  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-  vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-  vim.keymap.set('n', '<space>f', function()
-    vim.lsp.buf.format { async = true }
-  end, opts)
-end
+-- [[ Configure and install plugins ]]
+require 'lazy-plugins'
 
-local lspconfig = require('lspconfig')
-local servers = {
-  "terraformls",
-  "tflint",
-  "shellcheck",
-  "rust_analyzer",
-  "ansiblels"
-}
-
-for _,lsp in ipairs(servers) do
-  lspconfig[lsp].setup({
-    on_attach = on_attach,
-    capabilities = require('cmp_nvim_lsp').default_capabilities()
-  })
-end
-
--- luasnip setup
-local luasnip = require("luasnip")
-
--- nvim-cmp setup
-local cmp = require("cmp")
-cmp.setup({
-  snippet = {
-    expand = function(args)
-	    luasnip.lsp_expand(args.body)
-    end,
-  },
-  sources = {
-    { name = 'copilot' },
-    { name = 'nvim_lsp'},
-    { name = 'buffer' },
-    { name = 'path' }
-  },
-  mapping = {
-    ["<C-p>"] = cmp.mapping.select_prev_item(),
-    ["<C-n>"] = cmp.mapping.select_next_item(),
-    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<C-e>"] = cmp.mapping.close(),
-    ["<CR>"] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    }),
-    ["<Tab>"] = function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end,
-    ["<S-Tab>"] = function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end,
-  }
-})
-
--- telescope bindings
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
--- trouble bindings
-vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
-vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
-vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
-vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
-vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
-vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
-
--- nvimtree bindings
-vim.keymap.set('n', "<leader>e", function() require("nvim-tree.api").tree.toggle() end)
-
--- set theme
-vim.cmd.colorscheme "catppuccin-mocha"
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
