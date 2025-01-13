6cc2ede4ac47d87e2dab0b735a75021164790356
João Pinto <pinto@fatska.xyz>
2025-01-13 15:40:11 +0000

    yadm: clearer start and end logs for jetbrains and rust bootstrap

--- .config/yadm/bootstrap.d/80-jetbrains.sh
+++ .config/yadm/bootstrap.d/80-jetbrains.sh
@@ -4,6 +4,7 @@
 
 set -e
 
+printf "bootstrap.d - jetbrains start\n"
 printf "Starting Jetbrains Toolbox Install\n"
 
 INSTALL_DIR="$HOME/.local/share/JetBrains/Toolbox/bin"
@@ -13,7 +14,7 @@ printf "Downloading archive...\n"
 ARCHIVE_URL=$(curl -s 'https://data.services.jetbrains.com/products/releases?code=TBA&latest=true&type=release' | grep -Po '"linux":.*?[^\\]",' | awk -F ':' '{print $3,":"$4}'| sed 's/[", ]//g')
 ARCHIVE_FILENAME=$(basename "$ARCHIVE_URL")i
 rm "$TMP_DIR/$ARCHIVE_FILENAME" 2>/dev/null || true
-wget -q --show-progress -cO "/tmp/$ARCHIVE_FILENAME" "$ARCHIVE_URL"
+wget -q -cO "/tmp/$ARCHIVE_FILENAME" "$ARCHIVE_URL"
 
 printf "Extracting to installation directory...\n"
 mkdir -p "$INSTALL_DIR"
@@ -27,3 +28,5 @@ rm "$SYMLINK_DIR/jetbrains-toolbox" 2>/dev/null || true
 ln -s "$INSTALL_DIR/jetbrains-toolbox" "$SYMLINK_DIR/jetbrains-toolbox"
 
 printf "End Jetbrains Toolbox Install\n"
+printf "bootstrap.d - jetbrains end\n"
+
--- .config/yadm/bootstrap.d/80-rust.sh
+++ .config/yadm/bootstrap.d/80-rust.sh
@@ -2,8 +2,12 @@
 
 set -e
 
+printf "bootstrap.d - rust start\n"
+
 # Install Rust
 if [[ ! $(command -v rustup) ]]; then
   printf "Installing rustup\n"
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path
 fi
+
+printf "bootstrap.d - rust end\n"
