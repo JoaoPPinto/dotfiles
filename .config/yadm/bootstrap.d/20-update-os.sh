093a4ff905783df1ebf2a994a6e87c5c5d2b491d
João Pinto <pinto@cmapi.xyz>
2026-02-28 14:22:23 +0000

    yadm: add 20-update-os

---
+++ .config/yadm/bootstrap.d/20-update-os.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# 21-update-os
+# Updates the base OS packages
+
+set -euo pipefail
+
+stage "Updating base OS packages"
+
+case "${BOOTSTRAP_OS:-}" in
+    fedora)
+        print_msg INFO "Updating Fedora system packages..."
+        required_sudo
+        sudo dnf upgrade --assumeyes --quiet --refresh
+        ;;
+    ubuntu|debian)
+        print_msg INFO "Updating ${BOOTSTRAP_OS^} system packages..."
+        require_sudo
+        sudo apt update -y
+        sudo apt upgrade -y
+        ;;
+    *)
+        print_msg WARN "Base OS update not supported for $BOOTSTRAP_OS. Skipping..."
+        ;;
+esac
+
+print_msg INFO "Base OS packages update complete."
